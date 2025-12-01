95f9f48de3dcaa8d560d8800a4a7b2046d86da79
yusuf <yusuftalhaklc@gmail.com>
2025-12-01 16:07:08 +0300

    Add RepositoryResolver

---
+++ Project.Bll/DependencyResolvers/RepositoryResolver.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.Dal.Repositories.Abstracts;
+using Project.Dal.Repositories.EFConcretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Bll.DependencyResolvers
+{
+    public static class RepositoryResolver
+    {
+        public static void AddRepositoryService(this IServiceCollection services)
+        {
+            services.AddScoped<IAppUserRepository, AppUserRepository>();
+            services.AddScoped<IAppUserProfileRepository, AppUserProfileRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+    }
+}
